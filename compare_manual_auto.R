f9f7a57e7ad1840e0270a0f233addefbe1a2f9b6
Pit <pit@mpib-berlin.mpg.de>
2026-04-03 15:10:54 +0200

    added pilot 2 file

--- compare_manual_auto.R
+++ compare_manual_auto.R
@@ -1,15 +1,23 @@
 # combine automated and manual coding
+library(tidyverse)
 library(stringr)
 
 ## read manual coding file
-manual = read.csv("data/2025-12-09_Codingform_ PilotingAssessment_pilot.csv")
+manual1 = read.csv("data/2025-12-09_Codingform_PilotingAssessment_pilot.csv")
+manual1$coding = "pilot1"
+manual2 = read.csv("data/2026-04-03_Codingform_PilotingAssessment_pilot2.csv")
+manual2$coding = "pilot2"
+# combine both pilots
+manual = bind_rows(manual1, manual2)
 
 # make clean doi column, remove .org inclusion
 manual <- manual %>%
   mutate(
-    doi = X1.2..Article.DOI |> 
+    doi = X1.2..Article.DOI |>
       sub("^https?://doi\\.org/", "", x = _) |>
-      sub("(?i)^doi:\\s*", "", x = _, perl = TRUE)
+      sub("^doi\\.org/", "", x = _) |>
+      sub("(?i)^doi:\\s*", "", x = _, perl = TRUE) |>
+      str_trim()
   )
 # select dois with manual coding
 dois = manual$doi
@@ -30,7 +38,7 @@ comb = full_join(
   by = "doi"
 )
 
-write.csv(comb, "data/auto-and-manual-combined.csv", row.names = FALSE)
+write.csv(comb, "data/2026-04-03_auto-and-manual-combined.csv", row.names = FALSE)
 
 ## compare manual to auto coding
 
@@ -62,23 +70,24 @@ comb_fund %>%
   group_by(auto_extracted, manual_extracted) %>%
   summarise(n = n(),
             perc = n/nrow(comb_fund))
-# auto_extracted manual_extracted     n   perc
-# <lgl>          <lgl>            <int>  <dbl>
-# 1 FALSE          FALSE               10 0.154 
-# 2 FALSE          TRUE                18 0.277 
-# 3 TRUE           FALSE                2 0.0308
-# 4 TRUE           TRUE                35 0.538
+#   auto_extracted manual_extracted     n   perc
+#   <lgl>          <lgl>            <int>  <dbl>
+# 1 FALSE          FALSE               10 0.0671
+# 2 FALSE          TRUE                59 0.396 
+# 3 TRUE           FALSE                2 0.0134
+# 4 TRUE           TRUE                78 0.523
 
 ## inspect manually
 fund_inspect = 
   comb_fund %>%
   filter(manual_extracted, auto_extracted)
 ## added value auto is the dois and easily readable funding body names etc
+## NOTE: openalex extraction is missing; coding error or lack of information available?
 
 # compare COI info
 comb_coi = 
   comb %>%
-  select(doi, contains("coi"), contains("conflict"))
+  select(doi, contains("coi"), contains("conflict"), coding)
 
 ## add whether any statement or coding available at all (also includes "no coi" statements)
 comb_coi <- comb_coi %>%
@@ -86,31 +95,35 @@ comb_coi <- comb_coi %>%
     auto_extracted = if_any(starts_with("coi"), ~ 
                               !is.na(.) & str_trim(.) != "" & !str_to_lower(.) %in% c("na", "n/a")
     ),
-    manual_extracted = !is.na(.data[["X8.2.1..If.there.is.a.conflict.of.interest.statement..what.is.the.statement."]]) &
+    manual_extracted_content_available = !is.na(.data[["X8.2.1..If.there.is.a.conflict.of.interest.statement..what.is.the.statement."]]) &
       str_trim(.data[["X8.2.1..If.there.is.a.conflict.of.interest.statement..what.is.the.statement."]]) != "" &
       !str_to_lower(.data[["X8.2.1..If.there.is.a.conflict.of.interest.statement..what.is.the.statement."]]) %in% c("na", "n/a")
-  )
+  ) %>%
+  mutate(manual_does_include_coi = 
+           ifelse(coding == "pilot1", 
+                  X8.2..Does.the.article.include.a.statement.indicating.whether.there.were.any.conflicts.of.interest.,
+                  X8.2..Does.the.article.include.a.statement.indicating.whether.there.were.any.conflicts.of.interest..COI..)) 
 
 ## check amount of manual coding errors / missing
 comb_coi %>%
-  rename(manual_does_include_coi = "X8.2..Does.the.article.include.a.statement.indicating.whether.there.were.any.conflicts.of.interest.") %>%
+  #filter(coding == "pilot2") %>%
   group_by(manual_does_include_coi,
-           manual_extracted) %>%
+           manual_extracted_content_available) %>%
   summarise(n = n(),
-            perc = n/nrow(comb_fund))
+            perc = n/nrow(comb_coi))
 
-# manual_does_include_coi                                           manual_extracted     n   perc
-# <chr>                                                             <lgl>            <int>  <dbl>
-# 1 No, there is no conflict of interest statement                    FALSE               21 0.323 
-# 2 No, there is no conflict of interest statement                    TRUE                 2 0.0308
-# 3 Yes, the statement says that there are one or more conflicts of … FALSE                6 0.0923
-# 4 Yes, the statement says that there are one or more conflicts of … TRUE                 4 0.0615
-# 5 Yes, the statement says that there is no conflict of interest     FALSE               15 0.231 
-# 6 Yes, the statement says that there is no conflict of interest     TRUE                17 0.262
+# manual_does_include_coi                                                  manual_extracted_content_available     n   perc
+# <chr>                                                                    <lgl>                              <int>  <dbl>
+# 1 No, there is no conflict of interest statement                           FALSE                                 37 0.248 
+# 2 No, there is no conflict of interest statement                           TRUE                                   2 0.0134
+# 3 Yes, the statement says that there are one or more conflicts of interest FALSE                                  6 0.0403
+# 4 Yes, the statement says that there are one or more conflicts of interest TRUE                                  10 0.0671
+# 5 Yes, the statement says that there is no conflict of interest            FALSE                                 31 0.208 
+# 6 Yes, the statement says that there is no conflict of interest            TRUE                                  63 0.423 
 
 ## rough estimate of overlap in extractions
 comb_coi %>%
-  group_by(auto_extracted, manual_extracted) %>%
+  group_by(auto_extracted, manual_extracted_content_available) %>%
   summarise(n = n(),
             perc = n/nrow(comb_coi))
 # auto_extracted manual_extracted     n   perc
